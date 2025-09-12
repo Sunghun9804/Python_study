f9125eda225633da2b5712e251f88b8f6fcc6e9e
Jeong_sunghun <sunghun0406@naver.com>
2025-09-12 11:11:59 +0900

    dictionary 내용

--- 16_dictionary.py
+++ 16_dictionary.py
@@ -28,3 +28,9 @@ print(a)
 # key를 직접 입력
 print(dic2['name'])
 print(dic2['friends'])
+
+# get()함수 사용
+print(dic2.get('phone'))
+# 특정 키가 없는경우 None이 아닌 대체 내용으로 반환할수 있음
+print(dic2.get('nick')) # -> 실행하면 None 나옴
+print(dic2.get('nick','해당 내용이 없음')) # -> "해당 내용이 없음" 으로 나옴
