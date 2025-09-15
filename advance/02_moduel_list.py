8c85121543e52f4c409b705552e017516dddc006
Jeong_sunghun <sunghun0406@naver.com>
2025-09-15 12:37:14 +0900

    모듈사용 4

--- advance/02_moduel_list.py
+++ advance/02_moduel_list.py
@@ -6,3 +6,9 @@ print(dir(oper))
 # 모듈의 이름 확인
 print(oper.__name__) # 특정 모듈의 이름을 확인
 print(__name__) # 현재 나의 이름 -> 현재 실행되는 함수
+
+# run() jump() walk() swim() -> exercise
+
+# plus() # minus() multiple() -> calculation
+
+# acceleration() break() drift() -> drive
