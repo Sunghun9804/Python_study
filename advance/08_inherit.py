8ec1faba347009db80165c40ba3d155b08c6727d
Jeong_sunghun <sunghun0406@naver.com>
2025-09-16 12:08:02 +0900

    클래스 생성 마무리

--- advance/08_inherit.py
+++ advance/08_inherit.py
@@ -3,17 +3,18 @@ class Runner:
     #     pass             ----> 생성자 생략
 
     def run(self):
-        pass
+        print(f'달린다.')
 
     def sprint(self):
-        pass
+        print(f'전력질주를 한다.')
 
 class Jumper:
     def jump(self):
-        pass
+        print(f'점프를 한다.')
 
     def high_jump(self):
-        pass
+        print(f'높이 점프를 한다.')
 
-class Person(Jumper, Runner):
-    pass
+class Person(Jumper, Runner): # jumper 와 Runner 를 상속 받았다.
+    def walk(self):
+        print(f'걷는다.')
