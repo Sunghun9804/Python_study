b1e5d4aee40356dda56a5ff4dc4e4573d7e9c505
Jeong_sunghun <sunghun0406@naver.com>
2025-09-16 12:36:56 +0900

    생략된 부모 생성자

---
+++ advance/09_super_init.py
@@ -0,0 +1,10 @@
+class Parent():
+    def __init__(self):
+        print('부모 생성자 실행!')
+
+class Child(Parent):
+    def __init__(self):
+        super().__init__() # 생략된 부모 생성자
+        print('자식 생성자 실행!')
+
+ch = Child()
