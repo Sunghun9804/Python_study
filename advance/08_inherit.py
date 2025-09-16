14a56961ff1692d7cfa9589e5e4b018d3536c4b3
Jeong_sunghun <sunghun0406@naver.com>
2025-09-16 12:03:32 +0900

    러너만들기

---
+++ advance/08_inherit.py
@@ -0,0 +1,15 @@
+class Runner:
+    # def __init__(self):
+    #     pass             ----> 생성자 생략
+
+    def run(self):
+        pass
+
+    def sprint(self):
+        pass
+
+class Jumper:
+    pass
+
+class Person(Jumper, Runner):
+    pass
